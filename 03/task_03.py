337eb223635a06deaaad9005329bdbf7f9c0aab4
Albert <bachkovalbert@mail.ru>
2022-09-11 19:13:48 +0500

    CHANGED: 03(+task03)

--- 03/task_03.py
+++ 03/task_03.py
@@ -1,3 +1,22 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу
 # между максимальным и минимальным значением дробной части элементов.
 # Пример:  - [1.1, 1.2, 3.1, 10.01] => 0.19
+
+def List_creation(n):
+    list = []
+    for i in range(n):
+        element = input(f'Input {i+1} float element: ') 
+        list.append(element)
+    print(list)
+    return list
+
+def Difference(array):
+    new_list = []
+    for i in range(len(array)):
+        element = float(array[i]) % 1 
+        new_list.append(element)
+    return max(new_list) - min(new_list)
+
+size = int(input('Input number N: '))
+
+print(round(Difference(List_creation(size)), 2))
