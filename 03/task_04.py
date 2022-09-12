98b261bbeb642068b6fd01a00dea46259d9571d7
Albert <bachkovalbert@mail.ru>
2022-09-12 10:09:24 +0500

    CHANGED: 03(+task04)

--- 03/task_04.py
+++ 03/task_04.py
@@ -3,3 +3,13 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+def Transform(n):
+    t = ''
+    while n != 0:
+        t = str(n%2) + t
+        n //= 2
+    return t
+
+num = int(input('Input integer number: '))
+print(Transform(num))
