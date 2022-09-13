a5589a27a05ef43c09a8450fa5b58fcc7d0ee415
Albert <bachkovalbert@mail.ru>
2022-09-13 15:18:15 +0500

    CHANGED: 04(+task02)

--- 04/task_02.py
+++ 04/task_02.py
@@ -2,3 +2,15 @@
 # которая составит список простых множителей числа N.
 # "20" -> [2, 2, 5]
 
+def Simple_Multipliers(num):
+    i = 2 
+    list = []
+    while i <= num:
+        if num % i == 0:
+            list.append(i)
+            num /= i
+        else:
+            i += 1
+    return list
+
+print(Simple_Multipliers(20))
