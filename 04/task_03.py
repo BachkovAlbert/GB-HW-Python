a633a3134e493ee2d5c6e8d0fa1f5fd931f473ea
Albert <bachkovalbert@mail.ru>
2022-09-15 11:53:00 +0500

    CHANGED: 04(fixed 03)

--- 04/task_03.py
+++ 04/task_03.py
@@ -3,7 +3,7 @@
 # [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
 
 
-def deletion_of_repetitions(list):
+def elimination_of_repetitions(list):
     lst = []
     i = 0
     while i < len(list):
@@ -13,5 +13,5 @@ def deletion_of_repetitions(list):
         i += 1
     return lst
 
-print(deletion_of_repetitions([1, 1, 2, 3, 4, 5, 5]))
+print(elimination_of_repetitions([1, 1, 2, 3, 4, 5, 5]))
 
