a17294ecb6ec5f68566a5a7fdbbfdf2bdeabb96c
Albert <bachkovalbert@mail.ru>
2022-09-17 10:54:49 +0500

    CHANGED: 04(fixed 04)

--- 04/task_04.py
+++ 04/task_04.py
@@ -5,45 +5,45 @@
 import random
 
 def create_list(k):
-    list = []      
+    tsil = []      
     for i in range(k + 1):
-        list.append(random.randint(0, 100))
-    return list
+        tsil.append(random.randint(0, 100))
+    return tsil
     
 def polynomial_creation(l):
-    list= l[:]
+    tsil= l[:]
     new_list = ''
-    if len(list) < 1:
+    if len(tsil) < 1:
         new_list = 'x = 0'
     else:
-        for i in range(len(list)):
-            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-                if list[i] == 1:
-                    new_list += f'x^{len(list)-i-1}'
+        for i in range(len(tsil)):
+            if i != len(tsil) - 1 and tsil[i] != 0 and i != len(tsil) - 2:
+                if tsil[i] == 1:
+                    new_list += f'x^{len(tsil)-i-1}'
                 else:
-                    new_list += f'{list[i]}x^{len(list)-i-1}'
-                if list[i+1] != 0:
+                    new_list += f'{tsil[i]}x^{len(tsil)-i-1}'
+                if tsil[i+1] != 0:
                     new_list += ' + '
                 else: 
-                    while list[i+1] == 0 and i != len(list) - 2:
-                        if list[i + 1] == 0: 
+                    while tsil[i+1] == 0 and i != len(tsil) - 2:
+                        if tsil[i + 1] == 0: 
                             new_list += ''
                         i += 1
-                    if list[i+1] != 0:
+                    if tsil[i+1] != 0:
                         new_list += ' + '
                     
-            elif i == len(list) - 2 and list[i] != 0:
-                if list[i] == 1:
+            elif i == len(tsil) - 2 and tsil[i] != 0:
+                if tsil[i] == 1:
                     new_list += f'x'
                 else:    
-                    new_list += f'{list[i]}x'
-                if list[i+1] != 0:
+                    new_list += f'{tsil[i]}x'
+                if tsil[i+1] != 0:
                     new_list += ' + '
 
-            elif i == len(list) - 1 and list[i] != 0:
-                new_list += f'{list[i]} = 0'
+            elif i == len(tsil) - 1 and tsil[i] != 0:
+                new_list += f'{tsil[i]} = 0'
 
-            elif i == len(list) - 1 and list[i] == 0:
+            elif i == len(tsil) - 1 and tsil[i] == 0:
                 new_list += ' = 0'
                 
     return new_list
@@ -54,10 +54,10 @@ def write_file(f):
 
 k = int(input('input natural degree k: '))
 
-list = create_list(k)           
+tsil = create_list(k)           
 
-print(list)      
-print(polynomial_creation(list))    
+print(tsil)      
+print(polynomial_creation(tsil))    
 
-write_file(polynomial_creation(list))
+write_file(polynomial_creation(tsil))
 
