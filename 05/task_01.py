4120adacc6c262de814371a657d774e0050fc50c
Albert <bachkovalbert@mail.ru>
2022-09-18 10:27:05 +0500

    CHANGED: 05(+task_01)

--- 05/task_01.py
+++ 05/task_01.py
@@ -1,1 +1,11 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+text = 'Мы неабв очень любим Питон иабв Джавабв'
+
+text_find = 'абв'
+
+list1 = text.split(' ')
+
+list2 = [item for item in list1 if text_find not in item]
+
+print(list2)
